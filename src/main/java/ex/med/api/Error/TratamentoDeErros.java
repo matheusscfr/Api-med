8aafe86705f608da2841d9e66445449010173118
Matheus Ferreira <matheusscfr@gmail.com>
2024-07-25 20:36:25 -0300

    Autenticacao do Token pelo cabeçalho.

--- src/main/java/ex/med/api/Error/TratamentoDeErros.java
+++ src/main/java/ex/med/api/Error/TratamentoDeErros.java
@@ -1,7 +1,9 @@
 package ex.med.api.Error;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +20,10 @@ public class TratamentoDeErros {
     public ResponseEntity tratarError400(MethodArgumentNotValidException ex){
         var erros = ex.getFieldErrors();
         return ResponseEntity.badRequest().body(erros.stream().map(DadosErrosValidacao::new));
+    }
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity tratarErroAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
     }
 
     private record DadosErrosValidacao(String campo, String mensagem){
--- src/main/java/ex/med/api/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/ex/med/api/security/ConfiguracaoDeSeguranca.java
@@ -1,7 +1,9 @@
 package ex.med.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,15 +12,22 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracaoDeSeguranca {
 
+    @Autowired
+    private FiltroDeSeguranca filtroDeSeguranca;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        return http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().build();
+        return http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeRequests().requestMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated()
+                .and().addFilterBefore(filtroDeSeguranca, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
